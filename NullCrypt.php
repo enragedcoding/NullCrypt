93f94c4cceb2be1240c34ac833bc8d10dac2a156
NullPatrol <demon@ub3rmail.com>
2015-05-19 02:40:50 +0200

    Customizable 2-Way Function
    2-Way Encryption

--- NullCrypt.php
+++ NullCrypt.php
@@ -13,6 +13,15 @@ if ( !function_exists( 'hex2bin' ) ) {
     }
 }
 class NullCrypt {
+  function CheckUpdate($ML) {
+      if (file_exists('NullCrypt.version'))
+      if (file_get_contents('NullCrypt.version') != "https://raw.githubusercontent.com/NullPatrol/Secure-Password-Encryption-Function/master/NullCrypt.version") {
+      //send mail:
+        error_log("Newer Version of NullCrypt available at https://Github.com/NullPatrol/Secure-Password-Encryption-Function/", 1,$ML);
+        error_log("Newer Version of NullCrypt available at https://Github.com/NullPatrol/Secure-Password-Encryption-Function/", 0);
+      }
+  }
+  
   private function H2B($input){
     if (!is_string($input)) return null;
     $value = unpack('H*', $input);
@@ -26,20 +35,34 @@ class NullCrypt {
     $K = explode("\r\n", chunk_split($K, strlen($M), "\r\n"));
     return $K;
   }
-  
   private function A2B($M) {
     return str_pad(decbin(ord($M)), 8, "0", STR_PAD_LEFT);
   }
-  
   private function B2H($B) {
     return dechex(bindec($B));
   }
-  
   private function doXOR($B,$K) {
     if ($B xor $K)
       return "1";
     else 
       return "0";
+  }
+  function private C($M,$S,$V) {
+    global $CC;
+    for ($i=0;$i<2;$i++) {
+      foreach($CC as $MT) {
+      $M=openssl_encrypt($M,$MT,$S,0,$V);
+      }
+    }
+    return $M;
+  }
+  function private M($C,$S,$V) {
+    global $CC;
+    for($i=0;$i<2;$i++) {
+      foreach(array_reverse($CC) as $MT)
+      $C=openssl_decrypt($C,$MT,$S,0,$V);
+    }
+    return $C;
   }
   private function doAND($B,$K) {
     if ($B == $K)
@@ -105,17 +128,6 @@ class NullCrypt {
     return $C;
   }
   
-  function CheckUpdate($ML) {
-      if (file_exists('NullCrypt.version'))
-      if (file_get_contents('NullCrypt.version') != "https://raw.githubusercontent.com/NullPatrol/Secure-Password-Encryption-Function/master/NullCrypt.version") {
-      //send mail:
-        error_log("Newer Version of NullCrypt available at https://Github.com/NullPatrol/Secure-Password-Encryption-Function/", 1,$ML);
-        error_log("Newer Version of NullCrypt available at https://Github.com/NullPatrol/Secure-Password-Encryption-Function/", 0);
-      }
-      
-  }
-  
-  
   function Decrypt($C) {
     $H = explode(':',$C);
     $CB = $CS = $CDS = "";
@@ -131,6 +143,32 @@ class NullCrypt {
     $MCp= explode('$', $MC);
     $CM = crypt($this->P_E($M, $K), sprintf('$%s$%s$%s$', $MCp[1], $MCp[2], $MCp[3]));
     return var_export($CM === $MC, true);
+  }
+  
+  function Obfuscate($C,$K,$R) {
+    $n=substr(mt_rand(),0,1);
+    $SX= substr(md5(rand()),0,$n);
+    $V=mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC),MCRYPT_RAND);
+    for($i=0;$i<=$R;$i++) {
+      $C=gzcompress(C($C,$K,$V),9);
+    }  
+    $C=gzcompress(C($C,$SX,$V),9);
+    return utf8_encode($n.$SX.$C.$V);
+  }
+  
+  function DeObfuscate($C,$K,$R) {
+    $XA=$C[0];
+    $S=substr($C,1,$C[0]);
+    $C=substr($C,++$XA);
+    $IV=mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+    $V=substr(utf8_decode($C),-$IV);
+    $C=substr($C,0,-$IV);
+    
+    $C=M(gzuncompress(utf8_decode($C)),$S,$V);
+    for($i=0;$i<=$R;$i++) {
+      $C=M(gzuncompress($C),$K,$V);
+    }
+    return $C;
   }
 }
 
